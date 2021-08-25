765b759debed12816895ad99fe295be2b28529a2
Markus Enax <Markus.Enax@gmail.com>
2021-08-25 20:07:08 +0200

    Drop tryout code

--- lib/perf_test.ex
+++ lib/perf_test.ex
@@ -3,8 +3,6 @@ defmodule PerfTest do
 
   import DataTree.{Node, TreePath}
 
-  alias DataTree.{Node, TreePath}
-
   def start(_type, _args) do
     insert_many()
     {:ok, self()}
@@ -31,7 +29,7 @@ defmodule PerfTest do
     length(sub) |> IO.puts()
   end
 
-  def populate_ets_set do
+  def populate do
     IO.puts("DataTree.populate")
 
     DataTree.new(name: :ptree)
@@ -41,7 +39,6 @@ defmodule PerfTest do
     DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.puts()
 
     IO.puts("DataTree.subtree")
-    # Process.sleep(3000)
 
     start = DateTime.utc_now()
     {:ok, sub} = DataTree.subtree(:ptree, ~p"data")
@@ -49,110 +46,5 @@ defmodule PerfTest do
 
     length(sub) |> IO.puts()
     # sub |> IO.inspect()
-  end
-
-  def populate_sep_ets_set do
-    IO.puts("DataTreeSep.populate")
-
-    DataTreeSep.new(name: :ptree)
-
-    start = DateTime.utc_now()
-    DataTreeSep.populate(:ptree)
-    DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.puts()
-
-    IO.puts("DataTreeSep.subtree")
-    # Process.sleep(3000)
-
-    start = DateTime.utc_now()
-    {:ok, sub} = DataTreeSep.subtree(:ptree, ~p"data")
-    DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.puts()
-
-    length(sub) |> IO.puts()
-    # sub |> IO.inspect()
-  end
-
-  def populate_shards do
-    IO.puts("DataTreeShards.populate")
-
-    DataTreeShards.new(name: :ptree)
-
-    start = DateTime.utc_now()
-    DataTreeShards.populate(:ptree)
-    DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.puts()
-
-    IO.puts("DataTreeShards.subtree")
-    # Process.sleep(3000)
-
-    start = DateTime.utc_now()
-    {:ok, sub} = DataTreeShards.subtree(:ptree, ~p"data")
-    DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.puts()
-
-    length(sub) |> IO.puts()
-    # sub |> IO.inspect()
-  end
-
-  def populate_bag do
-    IO.puts("DataTreeBag.populate")
-
-    DataTreeBag.new(name: :ptree)
-
-    start = DateTime.utc_now()
-    DataTreeBag.populate(:ptree)
-    DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.puts()
-
-    IO.puts("DataTreeBag.subtree")
-
-    start = DateTime.utc_now()
-    {:ok, sub} = DataTreeBag.subtree(:ptree, ~p"data")
-    DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.puts()
-
-    length(sub) |> IO.puts()
-    # sub |> IO.inspect()
-  end
-
-  def populate_map do
-    IO.puts("DataTreeMap.populate")
-
-    m = DataTreeMap.new()
-
-    start = DateTime.utc_now()
-    m = DataTreeMap.populate(m)
-    DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.puts()
-
-    IO.puts("DataTreeMap.subtree")
-
-    start = DateTime.utc_now()
-    {:ok, sub} = DataTreeMap.subtree(m, ~p"data")
-    DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.puts()
-
-    length(sub) |> IO.puts()
-    # sub |> IO.inspect()
-  end
-
-  def bench_map do
-    m =
-      for i <- 1..100, j <- 1..100, k <- 1..20, into: %{} do
-        segments = ["data", Integer.to_string(i), Integer.to_string(j)]
-        node = TreePath.new(segments) |> Node.new("node_#{k}")
-        {Node.path(node), node}
-      end
-
-    map_size(m) |> IO.puts()
-  end
-
-  def bench_ets_set do
-    t = :ets.new(:ptree, [:named_table])
-
-    start = DateTime.utc_now()
-
-    for i <- 1..100, j <- 1..100, k <- 1..20 do
-      segments = ["data", Integer.to_string(i), Integer.to_string(j)]
-      node = TreePath.new(segments) |> Node.new("node_#{k}")
-      :ets.insert(t, {Node.path(node), node})
-    end
-
-    DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.puts()
-
-    :ets.tab2list(t) |> length() |> IO.puts()
   end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,6 @@ defmodule DataTree.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      # applications: [:shards],
       extra_applications: [:logger]
     ]
   end
@@ -22,8 +21,6 @@ defmodule DataTree.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_shards, "~> 0.2"},
-      {:ex2ms, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
