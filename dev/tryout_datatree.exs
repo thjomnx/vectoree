e9b659f63c2939fcefb51b6638ffc73801554a6c
Markus Enax <Markus.Enax@gmail.com>
2022-04-29 13:52:21 +0200

    Polish tryout code

--- dev/tryout_datatree.exs
+++ dev/tryout_datatree.exs
@@ -6,11 +6,44 @@ IO.puts("==> DataTree.populate")
 start = DateTime.utc_now()
 
 map =
-  for i <- 1..100, j <- 1..100, k <- 1..20, into: %{} do
+  for i <- 1..100, j <- 1..100, k <- 1..200, into: %{} do
     {~p"data.#{i}.#{j}.node_#{k}", Node.new(:int32, System.system_time(), :nanoseconds)}
   end
 
 DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.inspect(label: "Time [ms]")
+map_size(map) |> IO.inspect(label: "Size")
+
+# --------------------
+
+IO.puts("==> DataTree.normalize")
+start = DateTime.utc_now()
+
+map = DataTree.normalize(map)
+
+DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.inspect(label: "Time [ms]")
+map_size(map) |> IO.inspect(label: "Size")
+
+# --------------------
+
+IO.puts("==> DataTree.node")
+start = DateTime.utc_now()
+
+DataTree.node(map, ~p"data.23.42.node_11") |> IO.inspect(label: "PathNode tuple")
+
+DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.inspect(label: "Time [ms]")
+
+# --------------------
+
+IO.puts("==> DataTree.children")
+start = DateTime.utc_now()
+
+chln = DataTree.children(map, ~p"data.23.42")
+
+DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.inspect(label: "Time [ms]")
+map_size(chln) |> IO.inspect(label: "Size")
+
+# --------------------
+
 IO.puts("==> DataTree.subtree")
 start = DateTime.utc_now()
 
@@ -18,21 +51,10 @@ sub = DataTree.subtree(map, ~p"data")
 
 DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.inspect(label: "Time [ms]")
 map_size(sub) |> IO.inspect(label: "Size")
-IO.puts("==> DataTree.node")
-start = DateTime.utc_now()
 
-DataTree.node(map, ~p"data.23.42.node_11") |> IO.inspect(label: "PathNode tuple")
+# --------------------
 
-DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.inspect(label: "Time [ms]")
-IO.puts("==> DataTree.normalize")
-start = DateTime.utc_now()
-
-norm = DataTree.normalize(map)
-
-DateTime.utc_now() |> DateTime.diff(start, :millisecond) |> IO.inspect(label: "Time [ms]")
-map_size(norm) |> IO.inspect(label: "Size")
-
-IO.puts("==> DataTree.update")
+IO.puts("==> DataTree.update_status")
 start = DateTime.utc_now()
 
 up = DataTree.update_status(map, 128)
@@ -42,8 +64,9 @@ map_size(up) |> IO.inspect(label: "Size")
 {_p, v} = DataTree.node(up, ~p"data.23.42.node_11")
 IO.inspect(v.status, label: "Updated tuple status")
 
-IO.puts("==> DataTree.update_single")
+# --------------------
 
+IO.puts("==> DataTree.update_status (single)")
 path = ~p"data.23.42.node_11"
 start = DateTime.utc_now()
 
@@ -57,6 +80,8 @@ IO.inspect(v.modified, label: "Updated single tuple modified")
 {_p, v} = DataTree.node(up, ~p"data.23.42.node_12")
 IO.inspect(v.status, label: "Updated other tuple status")
 IO.inspect(v.modified, label: "Updated other tuple modified")
+
+# --------------------
 
 IO.puts("==> DataTree.delete")
 start = DateTime.utc_now()
