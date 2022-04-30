6e7e07f6c63955cd1aeeaece4ac696eee399051f
Markus Enax <Markus.Enax@gmail.com>
2022-04-30 21:37:40 +0200

    Drop old code and migrate benchmarks

--- perf/benchmarks_large.exs
+++ perf/benchmarks_large.exs
@@ -2,23 +2,24 @@ import DataTree.TreePath
 
 alias DataTree.Node
 
-for i <- 1..10,
-    j <- 1..10,
-    k <- 1..10,
-    l <- 1..10,
-    m <- 1..10,
-    n <- 1..10,
-    o <- 1..10,
-    p <- 1..2,
-    into: %{} do
-  {~p"data.#{i}.#{j}.#{k}.#{l}.#{m}.#{n}.#{o}.node_#{p}", Node.new()}
-end
+tree =
+  for i <- 1..10,
+      j <- 1..10,
+      k <- 1..10,
+      l <- 1..10,
+      m <- 1..10,
+      n <- 1..10,
+      o <- 1..10,
+      p <- 1..2,
+      into: %{} do
+    {~p"data.#{i}.#{j}.#{k}.#{l}.#{m}.#{n}.#{o}.node_#{p}", Node.new()}
+  end
 
-# Benchee.run(
-#   %{
-#     "update_value" => fn {table, path} -> NodeTable.update_value(table, path, 12345) end
-#   },
-#   inputs: %{
-#     "large_deep" => {:vt0, ~p"data.2.3.4.5.6.7.8.node_2"}
-#   }
-# )
+Benchee.run(
+  %{
+    "update_value" => fn {tree, path} -> DataTree.update_value(tree, path, 12345) end
+  },
+  inputs: %{
+    "large_deep" => {tree, ~p"data.2.3.4.5.6.7.8.node_2"}
+  }
+)
