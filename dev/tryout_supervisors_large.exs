2b10e843f0d8d4b66846536102ca8ff9e7288aa6
Markus Enax <Markus.Enax@gmail.com>
2023-08-25 13:54:25 +0200

    Increase large scenario

--- dev/tryout_supervisors_large.exs
+++ dev/tryout_supervisors_large.exs
@@ -57,7 +57,7 @@ defmodule CustomTimedSource do
     TreeServer.mount_source(mount_path)
 
     tree =
-      for i <- 1..250, into: %{} do
+      for i <- 1..1500, into: %{} do
         {~p"node_#{i}", Payload.new(:int32, System.system_time(), :nanosecond)}
       end
 
@@ -168,7 +168,7 @@ DynamicSupervisor.count_children(TreeSinkSupervisor) |> IO.inspect(label: "sinks
 
 # ---
 
-TreeServer.query(server_pid, ~p"data", chunk_size: 100)
+TreeServer.query(server_pid, ~p"data", chunk_size: 1000)
 |> Map.new(fn {k, v} -> {to_string(k), Payload.format(v)} end)
 |> IO.inspect(label: "query on 'data'")
 
