3845b8882a33917283741c4cdd95e4b8424094e5
Markus Enax <Markus.Enax@gmail.com>
2023-08-30 10:20:32 +0200

    Add more unit test code

--- test/vectoree/tree_server_test.exs
+++ test/vectoree/tree_server_test.exs
@@ -1,13 +1,10 @@
 defmodule Vectoree.TreeServerTest do
   use ExUnit.Case
 
-  alias Vectoree.TreeSource
   alias Vectoree.TreeServer
-  alias Vectoree.{Tree, TreePath}
+  alias Vectoree.TreePath
 
   @moduletag :capture_log
-
-  doctest Tree
 
   defmodule TestSource do
     use Vectoree.TreeSource
@@ -18,6 +15,31 @@ defmodule Vectoree.TreeServerTest do
       TreeServer.mount_source(mount_path)
 
       {:ok, %{mount_path: mount_path, local_tree: %{}}}
+    end
+  end
+
+  defmodule TestProcessor do
+    use Vectoree.TreeProcessor
+
+    @impl GenServer
+    def init(init_arg) do
+      %{mount: mount_path, listen: listen_path} = TreeServer.args2info(init_arg)
+      TreeServer.mount_source(mount_path)
+      TreeServer.register_sink(listen_path)
+
+      {:ok, %{mount_path: mount_path, local_tree: %{}}}
+    end
+  end
+
+  defmodule TestSink do
+    use Vectoree.TreeSink
+
+    @impl GenServer
+    def init(init_arg) do
+      %{listen: listen_path} = TreeServer.args2info(init_arg)
+      TreeServer.register_sink(listen_path)
+
+      {:ok, 0}
     end
   end
 
@@ -38,20 +60,34 @@ defmodule Vectoree.TreeServerTest do
     assert TreeServer.args2info(%{mount: mpath}) == %{mount: mpath}
     assert TreeServer.args2info(%{listen: lpath}) == %{listen: lpath}
     assert TreeServer.args2info(%{mount: mpath, listen: lpath}) == %{mount: mpath, listen: lpath}
-    assert TreeServer.args2info(%{mount: mpath, listen: lpath, foo: :bar}) == %{mount: mpath, listen: lpath}
+
+    assert TreeServer.args2info(%{mount: mpath, listen: lpath, foo: :bar}) == %{
+             mount: mpath,
+             listen: lpath
+           }
   end
 
-  test "start_test_source", context do
+  test "start source", context do
     server = context[:server]
 
     {:ok, pid} = TreeServer.start_source(server, TestSource, TreePath.new(["a", "r"]))
     assert is_pid(pid)
   end
 
-  # test "start_plain_source", context do
-  #   server = context[:server]
+  test "start processor", context do
+    server = context[:server]
+    mpath = TreePath.new(["a", "r"])
+    lpath = TreePath.new(["a", "l"])
 
-  #   {:ok, pid} = TreeServer.start_source(server, TreeSource, TreePath.new(["a", "r"]))
-  #   assert is_pid(pid)
-  # end
+    {:ok, pid} = TreeServer.start_processor(server, TestProcessor, mpath, lpath)
+    assert is_pid(pid)
+  end
+
+  test "start sink", context do
+    server = context[:server]
+    lpath = TreePath.new(["a", "l"])
+
+    {:ok, pid} = TreeServer.start_sink(server, TestSink, lpath)
+    assert is_pid(pid)
+  end
 end
