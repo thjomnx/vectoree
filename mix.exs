3c9c5e8a9c86092c63d3c223b230fe944f56436f
Markus Enax <Markus.Enax@gmail.com>
2023-09-20 21:22:11 +0200

    Add ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -25,6 +25,7 @@ defmodule Vectoree.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:benchee, "~> 1.1", only: :dev}
     ]
   end
