1b06e050c4a6196c1a75d1c8d1c2eff04dbeeff6
Markus Enax <Markus.Enax@gmail.com>
2023-09-20 21:19:30 +0200

    Drop logger from extra_apps

--- mix.exs
+++ mix.exs
@@ -19,9 +19,7 @@ defmodule Vectoree.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
