332573bd9d2468b802f837995db25adcc8dee70c
Markus Enax <Markus.Enax@gmail.com>
2024-01-21 17:36:41 +0100

    Add helper code to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,2 +1,19 @@
 alias Vectoree.{Node, TreePath, TreeServer}
 import Vectoree.TreePath
+
+c0 = ~p"a.b.c0"
+c1 = ~p"a.b.c1"
+b = TreePath.parent(c0)
+a = TreePath.root(b)
+
+t = %{
+  c0 => :foo,
+  c1 => :bar
+}
+
+IO.puts("\nExample data in this session from .iex.exs:")
+IO.puts("a :: TreePath: '#{a}'")
+IO.puts("b :: TreePath: '#{b}'")
+IO.puts("c0 :: TreePath: '#{c0}'")
+IO.puts("c1 :: TreePath: '#{c1}'")
+IO.inspect(t, label: "t :: Map")
